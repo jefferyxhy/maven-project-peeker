10701c9678d93d4a4869542c4b79f62d7b26846e
Jeffery Xie <jxie2@atlassian.com>
2025-05-26 16:43:52 +1000

    update plugin configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,9 @@ intellij {
   version.set("2023.2.6")
   type.set("IC") // Target IDE Platform
 
-  plugins.set(listOf(/* Plugin Dependencies */))
+  plugins.set(listOf(
+    "maven"
+  ))
 }
 
 tasks {
